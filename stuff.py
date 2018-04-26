eca15ba15f466648199dc61ca2d9a73524340014
Krishna Kumar <3963513+kks32@users.noreply.github.com>
2018-04-26 21:46:12 +0100

    :computer: Read fasta files

---
+++ stuff.py
@@ -0,0 +1,26 @@
+from Bio import SeqIO
+import argparse
+import gzip
+import regex
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Extract genes from reads')
+    parser.add_argument('file1', metavar="<FILE1>",
+                        help='file containing reads')
+
+    args = parser.parse_args()
+    r1 = make_read_list(args.file1)
+
+    #for i in range(len(r1)):
+    print(r1[0])
+        
+def make_read_list(file):
+    reads = []
+    handle = open(file, 'rU')
+    for record in SeqIO.parse(handle, "fasta"):
+        reads.append(str(record.seq))
+    return(reads)
+
+if __name__ == '__main__':
+    main()
